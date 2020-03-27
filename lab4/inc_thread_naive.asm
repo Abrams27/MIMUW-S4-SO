f97ed951b4716255e30686bd55492827c51a7a5c
Abrams27 <abramowiczmarcin99@gmail.com>
2020-03-27 20:29:05 +0100

    c4

--- lab4/inc_thread_naive.asm
+++ lab4/inc_thread_naive.asm
@@ -11,23 +11,19 @@ align 8
 inc_thread:
     mov     rsi, [rdi]      ; value
     mov     ecx, [rdi + 8]  ; count
-
     mov     rdx, spin_lock  ; W rdx jest adres blokady.
-    mov     edi, 1          ; W edi jest wartość zamknietej blokady.
-    jmp     count_test
 
-    mov     rdx, spin_lock
     jmp     count_test
 count_loop:
+    xor eax, eax
 busy_wait:
-    xor     eax, eax        ; W eax jest wartość otwartej blokady.
     lock \
-    cmpxchg [rdx], edi      ; Jeśli blokada otwarta, zamknij ją.
-    jne     busy_wait       ; Skocz, gdy blokada była zamknięta.
+    bts     [rdx], eax      ; Jeśli blokada otwarta, zamknij ją.
+    jc      busy_wait       ; Skocz, gdy blokada była zamknięta.
 
     inc     dword [rsi]     ; ++*value
 
-    mov     [rdx], eax      ; Otwórz blokadę.
+    btr     [rdx], eax      ; Otwórz blokadę.
 count_test:
     sub     ecx, 1          ; --count
     jge     count_loop      ; skok, gdy count >= 0
