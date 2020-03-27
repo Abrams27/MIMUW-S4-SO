a4efee126a2c0e3a7183be1c87d8905ea577fcd4
Abrams27 <abramowiczmarcin99@gmail.com>
2020-03-27 20:02:38 +0100

    c2

--- lab4/inc_thread_naive.asm
+++ lab4/inc_thread_naive.asm
@@ -1,17 +1,31 @@
 global inc_thread
 
+section .bss
+
+alignb 4
+spin_lock resd 1 ; 1 raz 32 bity
+
 section .text
 
 align 8
 inc_thread:
-  mov     rsi, [rdi]      ; value
-  mov     ecx, [rdi + 8]  ; count
-  jmp     count_test
+    mov     rsi, [rdi]      ; value
+    mov     ecx, [rdi + 8]  ; count
+
+    mov     rdx, spin_lock
+    jmp     count_test
 count_loop:
-  lock \
-  inc     dword [rsi]     ; ++*value
+    mov     eax, 1
+busy_wait:
+    xchg    [rdx], eax      ; Jeśli blokada otwarta, zamknij ją.
+    test    eax, eax        ; Sprawdź, czy blokada była otwarta.
+    jnz     busy_wait       ; Skocz, gdy blokada była zamknięta.
+
+    inc     dword [rsi]     ; ++*value
+
+    mov     [rdx], eax      ; Otwórz blokadę.
 count_test:
-  sub     ecx, 1          ; --count
-  jge     count_loop      ; skok, gdy count >= 0
-  xor     eax, eax        ; return NULL
-  ret
+    sub     ecx, 1          ; --count
+    jge     count_loop      ; skok, gdy count >= 0
+    xor     eax, eax        ; return NULL
+    ret
